b35c23ad44ffbdd549cd32b5c22f3a58812d112f
faisalalm01 <faisalali2858@gmail.com>
2022-08-04 18:01:51 +0700

    test

---
+++ src/pages/competition/webDesaign.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class webDesaign extends Component {
+  render() {
+    return (
+      <div>webDesaign</div>
+    )
+  }
+}
---
+++ src/pages/workshop/workshop.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function workshop() {
+  return (
+    <div>workshop</div>
+  )
+}
